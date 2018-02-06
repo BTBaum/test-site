fbb14619cb47df5c57e44078dfc844225a48ae86
Brandon <railsdev14@gmail.com>
2018-02-05 19:39:47 -0500

    Added axios for blogger content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+const blogger = 'https://www.googleapis.com/blogger/v3/blogs/1360640620050765862/posts?key=AIzaSyBq0Du33QhO9heRdHdlwol3oC_d0bVNjBc'
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      post: ''
+    }
+  }
+
+  componentDidMount() {
+    axios.get(blogger)
+      .then((res) => {
+        console.log("res.items[0].content", res.data.items[0].content)
+        this.setState({ post: res.data.items[0].content })
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +31,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        { this.state.post }
       </div>
     );
   }
